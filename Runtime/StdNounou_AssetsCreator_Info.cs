c06f23fba848b35b2c4612c7dadd9f06daf4c441
Florian <florianhusson@hotmail.fr>
2024-01-26 16:14:06 +0100

    namespace fix

--- Runtime/StdNounou_AssetsCreator_Info.cs
+++ Runtime/StdNounou_AssetsCreator_Info.cs
@@ -1,8 +1,7 @@
-
 namespace StdNounou.AssetsCreator
 {
 	public static class StdNounou_AssetsCreator_Info
 	{
-		public const string VERSION = "1.0.0";
+		public const string VERSION = "1.0.1";
     }
 }
